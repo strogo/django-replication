01b56479ec80e72131b71202bdaa2840c988b281
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2010-12-27 16:26:04 -0400

    Debug messages are conditional to the DEBUG variable

--- apps/replicate/debug.py
+++ apps/replicate/debug.py
@@ -1,5 +1,7 @@
 import sys, os
 
+from django.conf import settings
+
 def debug(msg):
     #print >> sys.stderr, "DEBUG: %s - %s" % (os.path.basename(__file__), msg)
     print >> sys.stderr, "DEBUG: %s" % msg

f8df7e912ad4be937b159dd3aab208ced400cd5e
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2010-12-28 14:36:50 -0400

    Used Django's new readonly admin fields, remove the custom implementation

--- apps/replicate/admin.py
+++ apps/replicate/admin.py
@@ -30,41 +30,6 @@ def clone_objects(objects, title_fieldnames):
         obj.save()
         objs.append(obj)
    
-        
-class ReadOnlyWidget(forms.Widget):
-    def __init__(self, original_value, display_value):
-        self.original_value = original_value
-        self.display_value = display_value
-
-        super(ReadOnlyWidget, self).__init__()
-
-    def render(self, name, value, attrs=None):
-        if self.display_value is not None:
-            return unicode(self.display_value)
-        return unicode(self.original_value)
-
-    def value_from_datadict(self, data, files, name):
-        return self.original_value
-
-
-class ReadOnlyAdminFields(object):
-    def get_form(self, request, obj = None):
-        form = super(ReadOnlyAdminFields, self).get_form(request, obj)
-
-        if hasattr(self, 'readonly'):
-            for field_name in self.readonly:
-                if field_name in form.base_fields:
-
-                    if hasattr(obj, 'get_%s_display' % field_name):
-                        display_value = getattr(obj, 'get_%s_display' % field_name)()
-                    else:
-                        display_value = None
-
-                    form.base_fields[field_name].widget = ReadOnlyWidget(getattr(obj, field_name, ''), display_value)
-                    form.base_fields[field_name].required = False
-
-        return form
-
 
 class Conduit_SetAdmin(admin.ModelAdmin):
     list_display = ['setname', 'concurrent', 'get_conduits']
@@ -84,14 +49,14 @@ class Conduit_SetAdmin(admin.ModelAdmin):
     execute.short_description = _(u"Manually execute conduit set")		
 
 
-class LogAdmin(ReadOnlyAdminFields, admin.ModelAdmin):
+class LogAdmin(admin.ModelAdmin):
     date_hierarchy = 'timestamp'
     list_filter = ['severity', 'module']
     list_display = ['id', 'timestamp', 'module', 'severity', 'message']
     search_fields = ['message']
     search_fields_verbose = ['Message']
     #list_display_links = ()
-    readonly = ['severity', 'module', 'message']
+    readonly_fields = ['severity', 'module', 'message']
     order = 5
     actions = None
 
